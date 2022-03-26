5df0fc321465aefcceec3dee3b5a1ab013b8ab64
Guilherme Oliveira <102302646+Gui-ODSC@users.noreply.github.com>
2022-03-26 11:50:10 -0300

    Add files via upload

---
+++ Movendo_item_de_uma_lista.py
@@ -0,0 +1,29 @@
+lista1 = []
+elemento = int(input("Escreva um elemento da lista: "))
+lista1.append(elemento)
+while elemento >= 0:
+    elemento = int(input("Escreva um elemento da lista: "))
+    if elemento >= 0:
+        lista1.append(elemento)
+for i in range(0, len(lista1)):
+    print(i, lista1[i])  
+lista2 = []
+n1 = int(input("Escreva um índice da lista: "))
+n2 = int(input("Escreva outro elemento da lista: "))
+
+for i in range(0, n1):
+    lista2.append(lista1[i])
+    print(i, lista2[i])
+    
+for i in range(n1, n2):
+    guarda = lista1[n1]
+    lista2.append(lista1[i+1])
+    print(i, lista2[i])
+    
+lista1[n2] = lista1[n1]
+lista2.append(lista1[n2])
+print(n2, lista2[n2])
+
+for i in range((n2+1) , len(lista1)):
+    lista2.append(lista1[i])
+    print(i, lista2[i])
